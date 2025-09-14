d70f064bce408c919077fd3d919a31b72d877910
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-14 22:36:14 +0530

    Create movies.js

---
+++ src/movies.js
@@ -0,0 +1,9 @@
+// src/movies.js
+const movies = [
+  { id: 1, title: "Stranger Things", image: "https://i.imgur.com/2g4X1.jpg" },
+  { id: 2, title: "Money Heist", image: "https://i.imgur.com/Q1XK0.jpg" },
+  { id: 3, title: "Wednesday", image: "https://i.imgur.com/4sZ3s.jpg" },
+  { id: 4, title: "Squid Game", image: "https://i.imgur.com/f3Z0k.jpg" }
+]
+
+export default movies
