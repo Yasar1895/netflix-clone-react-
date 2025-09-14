9a38828e5f9c861c6c874d426338d3b448b8aa94
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-14 23:09:28 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+// src/components/Navbar.jsx
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">Netflix Clone</h1>
+    </nav>
+  );
+}
